118caea88732dad2ca216b36465ebee2984f5856
billwk254 <billkihungu51@gmail.com>
2023-06-08 12:38:03 +0300

    Function that prints calculations after importing calculation function

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 
-import calculator_1 as calc
+from calculator_1 import add, sub, mul, div
 
 def calc_results():
 
 	a = 10
 	b = 5
+	result_add = add(a, b)
+	result_sub = sub(a, b)
+	result_mul = mul(a, b)
+	result_div = div(a, b)
 
-	sum_result = calc.add(a, b)
-	diff_result = calc.sub(a, b)
-	prod_result = calc.mul(a, b)
-	quot_result = calc.div(a, b)
+	print("{} + {} = {}".format(a, b, result_add))
+	print("{} - {} = {}".format(a, b, result_sub))
+	print("{} * {} = {}".format(a, b, result_mul))
+	print("{} / {} = {}".format(a, b, result_div))
 
-	print(f"Sum: {sum_result}")
-	print(f"Difference: {diff_result}")
-	print(f"Product: {prod_result}")
-	print(f"Quotient: {quot_result}")
 
 if __name__ == "__main__":
 	calc_results()
