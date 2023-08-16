8e3c3e5469f21b15ee5c4ceeff0d557071a622ba
billwk254 <billkihungu51@gmail.com>
2023-08-16 03:56:20 +0300

    Import in hbtn_0c_0 database

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- Calculate and display average temperature (Fahrenheit) by city ordered by temperature (descending)
+SELECT city, AVG(temp_fahrenheit) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
