b17ccb0a0c27fae8734558f45b20aaa0f3b709f8
billwk254 <billkihungu51@gmail.com>
2023-09-17 17:07:32 +0300

    a script that lists all City objects from the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -2,30 +2,28 @@
 
 
 """
-Script that lists all City objects from the database hbtn_0e_101_usa.
+Script that lists all City objects from the database hbtn_0e_101_usa..
 """
 
 
-import sys
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
+from sys import argv
 
 
-if __name__ == "__main__":
+def main():
     # Check if the script is called with the correct number of arguments
-    if len(sys.argv) != 4:
-        print("Wrong code format")
-        sys.exit(1)
+    if len(argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(argv[0]))
+        return
 
-    # Get the arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    mysql_username, mysql_password, database_name = argv[1], argv[2], argv[3]
 
     # Create an SQLAlchemy engine to connect to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
         mysql_username, mysql_password, database_name), pool_pre_ping=True)
 
     # Create all tables in the database
@@ -35,7 +33,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query to retrieve all City objects.
+    # Query to retrieve all City objects
     cities = session.query(City).order_by(City.id).all()
 
     # Loop through the cities and print the results
@@ -44,3 +42,6 @@ if __name__ == "__main__":
 
     # Close the session
     session.close()
+
+if __name__ == "__main__":
+    main()
