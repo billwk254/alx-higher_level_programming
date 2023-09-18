29868326ca5f4e756f0dd3e7d0880aa1438b44c9
billwk254 <billkihungu51@gmail.com>
2023-09-18 04:54:22 +0300

    Corrections to 15

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -7,43 +7,24 @@ contained in the database hbtn_0e_101_usa.
 """
 
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
+from sys import argv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    # Check if the script is called
-    if len(sys.argv) != 4:
-        print("Wrong code format")
-        sys.exit(1)
-
-    # Get the arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Create an SQLAlchemy engine to connect to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        mysql_username, mysql_password, database_name), pool_pre_ping=True)
-
-    # Create all tables in the database
-    Base.metadata.create_all(engine)
-
-    # Create a session to interact with the database
-    Session = sessionmaker(bind=engine)
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-
-    # Query to retrieve all State and City objects
-    states = session.query(State).order_by(State.id).all()
-
-    # Loop through the states and cities and print the results
-    for state in states:
+    rows = session.query(State).all()
+    for state in rows:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
-
-    # Close the session
+            print("    {}: {}".format(city.id, city.name))
     session.close()
