aeb244e6a8a25f3c334bb8248bb5c09f9b32c0f0
billwk254 <billkihungu51@gmail.com>
2023-09-18 04:45:09 +0300

    Corrections to task 17

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -20,28 +20,27 @@ def main():
         print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(argv[0]))
         return
 
+    # Extract command-line arguments
     mysql_username, mysql_password, database_name = argv[1], argv[2], argv[3]
 
     # Create an SQLAlchemy engine to connect to the database
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-        mysql_username, mysql_password, database_name), pool_pre_ping=True)
-
-    # Create all tables in the database
+    engine = create_engine(f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost/{database_name}")
     Base.metadata.create_all(engine)
 
     # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query to retrieve all City objects
+    # Query to retrieve all City objects, sorted by cities.id, with their associated State
     cities = session.query(City).order_by(City.id).all()
 
     # Loop through the cities and print the results
     for city in cities:
-        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+        print(f"{city.id}: {city.name} -> {city.state.name}")
 
     # Close the session
     session.close()
+
 
 if __name__ == "__main__":
     main()
