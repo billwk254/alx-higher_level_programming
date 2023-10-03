f2506d95ea1c191b520fca33c2b9b39affd02ac7
billwk254 <billkihungu51@gmail.com>
2023-10-03 17:29:56 +0300

    corrections

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,16 @@
 #!/usr/bin/node
+
 const fs = require('fs');
+const filePath1 = process.argv[2];
+const filePath2 = process.argv[3];
+const destinationPath = process.argv[4];
 
-const file1 = fs.readFileSync(process.argv[2]).toString();
+try {
+  const data1 = fs.readFileSync(filePath1, 'utf8');
+  const data2 = fs.readFileSync(filePath2, 'utf8');
 
-const file2 = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], file1 + file2);
+  const concatenatedData = data1 + data2;
+
+  fs.writeFileSync(destinationPath, concatenatedData);
+}
+
